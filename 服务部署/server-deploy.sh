a23a286e9c494a68df4464baefb46e874eee9fce
fjj <769434358@qq.com>
2020-04-22 18:02:10 +0800

    代码提交

--- 服务部署/server-deploy.sh
+++ 服务部署/server-deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-cd /usr/local/AllDemo/
+cd /root/AllDemo/
 echo "拉取最新代码"
 git pull
-cd demo2-usermanager
+cd /root/AllDemo/demo1-helloworld
 echo "编译打包项目"
 mvn clean install package
 
 echo "检查现有进程是否启动，并进行停止"
-pid=$(ps x | grep "demo2-usermanager-1.0.jar" | grep -v grep | awk '{print $1}')
+pid=$(ps x | grep "demo1-helloworld-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $1}')
 if [ -n "$pid" ]; then
 echo "存在对应进程，正在停止"
 kill -9 $pid
@@ -15,9 +15,11 @@ sleep 2s
 fi
 
 echo "拷贝jar包"
-cd /usr/local/demo2-usermanager-deploy/
-\cp -rf /usr/local/AllDemo/demo2-usermanager/target/demo2-usermanager-1.0.jar /usr/local/demo2-usermanager-deploy/
+cd /root/demo1-helloworld-deploy/
+\cp -rf /root/AllDemo/demo1-helloworld/target/demo1-helloworld-0.0.1-SNAPSHOT.jar /root/demo1-helloworld-deploy/
 echo "启动jar包"
-nohup java -jar demo2-usermanager-1.0.jar >/dev/null 2>&1 &
+#nohup java -jar demo1-helloworld-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+nohup java -jar demo1-helloworld-0.0.1-SNAPSHOT.jar &
 echo "查看服务器启动日志"
-tailf /usr/local/demo2-usermanager-deploy/logs/spring.log
+#tailf /root/demo1-helloworld-deploy/logs/spring.log
+tailf /root/demo1-helloworld-deploy/nohup.out
